5a1bcff87c5ebfddc4cdb298e590d6422f07199e
xudong <xudong@360.net>
2020-03-29 01:54:54 +0800

    修改应用权限

--- app/config.py
+++ app/config.py
@@ -2,8 +2,10 @@ class Config(object):
     SECRET_KEY = "CantStopAddictedToTheShinDigChopTopHeSaysImGonnaWinBig"
     HOST = "www.ycadvisor.com"
     SHOPIFY_CONFIG = {
-        'API_KEY': '5621465025cca3faec8def69d539539e',
-        'API_SECRET': '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',
+        #'API_KEY': '5621465025cca3faec8def69d539539e',
+        'API_KEY': '03e5e92a9189276630e50f24b17f0dc4',
+        #'API_SECRET': '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',
+        'API_SECRET': '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',
         'APP_HOME': 'https://' + HOST,
         'CALLBACK_URL': 'https://' + HOST + '/install',
         'REDIRECT_URI': 'https://' + HOST + '/connect',
